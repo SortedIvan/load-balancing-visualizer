f2ad04dd6c9e6211166f205b3d1d096db8208b7a
Ivan <ivanovcharov20@gmail.com>
2025-02-22 22:38:18 +0100

    init: Pushing initial project structure and base line class

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+    sf::RenderWindow window(
+        sf::VideoMode(1500, 900),
+        "Loadbalancing visualizer");
+
+    sf::Event e;
+
+
+    // Main loop
+    while (window.isOpen())
+    {
+
+        while (window.pollEvent(e))
+        {
+            if (e.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (e.type == sf::Event::TextEntered)
+            {
+
+            }
+        }
+
+        // Run updates
+
+
+        // Clear the window
+        window.clear(sf::Color::Black);
+
+        // draw
+
+        // display
+        window.display();
+    }
+}
